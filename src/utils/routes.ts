56de8063d2add2d33f19e25a16d253c07e10dc62
Vladimir Volek <volek.vladimir@gmail.com>
2022-11-06 17:44:34 +0100

    fix: testnet networks

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -12,3 +12,17 @@ export const parseBatch = (batch: Batch) => {
     tx_hash: batch.tx_hash,
   };
 };
+
+export const isTestnet = (projectId?: string) => {
+  if (!projectId) return false;
+
+  if (
+    projectId.includes('testnet') ||
+    projectId.includes('preview') ||
+    projectId.includes('preprod')
+  ) {
+    return true;
+  }
+
+  return false;
+};
